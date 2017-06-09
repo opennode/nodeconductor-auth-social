1af3e895b41c5d5b876cb2deab1787b2ec6a8e8b
Victor Mireyev <victor@opennodecloud.com>
2017-06-09 11:31:49 +0300

    Add license metadata.

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     url='http://waldur.com',
     description='Waldur plugin for authentication with social networks such as Facebook, Google Plus.',
     long_description=open('README.rst').read(),
+    license='MIT',
     package_dir={'': 'src'},
     packages=find_packages('src', exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
     install_requires=install_requires,
